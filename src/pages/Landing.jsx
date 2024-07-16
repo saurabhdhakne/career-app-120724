7e08d673b4341961613d2467dec3c6f69201b41a
saurabhdhakne <sbdhakne309@gmail.com>
2024-07-16 09:29:31 +0530

    ck editor added

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import { Link } from "react-router-dom";
 import { LandingSection } from "../components/LandingSection";
 import AboutSection from "../components/AboutSection";
 import ContactSection from "../components/ContactSection";
+import EditorSection from "../components/EditorSection";
 
 const Landing = () => {
   return (
